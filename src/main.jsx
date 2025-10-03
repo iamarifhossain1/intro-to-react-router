78bacc0f8a584f40c83ba87a57c363c8cd8923ba
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-10-04 03:21:17 +0600

    react-router

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,42 @@ import App from './App.jsx'
 
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import Root from './Components/Root/Root.jsx';
+import Home from './Components/Home/Home.jsx';
+import Mobile from './Components/Mobile/Mobile.jsx';
+import Laptop from './Components/Laptop/Laptop.jsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Hello from React Router</div>
+    Component: Root,
+    children: [
+      {index: true, Component: Home},
+      {path: 'Mobile', Component: Mobile},
+      {path: 'Laptop', Component: Laptop}
+    ]
+  },
+
+  {
+    path: 'home',
+    element: <div>Home</div>
+  },
+
+  {
+    path: 'profile',
+    element: <div>Profile</div>
+  },
+
+  {
+    path: 'about',
+    element: <div>About Me</div>
+  },
+
+  {
+    path: 'app',
+    Component: App
   }
-])
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
