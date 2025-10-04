aec431f88a66a7f212ab5abe930fd4b15a6126b3
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-10-04 13:02:43 +0600

    active-navlink

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router';
+import './Header.css'
 
 const Header = () => {
     return (
         <div>
             <h1>This is our Header</h1>
+
+            <nav>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/Mobile">Mobile</NavLink>
+                <NavLink to="/Laptop">Laptop</NavLink>
+            </nav>
         </div>
     );
 };
